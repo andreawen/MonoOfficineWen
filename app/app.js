0edf461ed253145e85922ba60c7bc5d36c75df6e
andreawen <wen.andrea@iisgalvanimi.edu.it>
2019-06-05 11:32:52 +0000

    .

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var app = express();
 const cors = require('cors')
 var bodyParser = require('body-parser')
 var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 
 app.use(cors());
 app.set('views', './views');
@@ -11,9 +12,6 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-    res.render("login");
-});
 
 app.get('/login/:login/:pass', function (req, res) {
       MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
@@ -71,7 +69,7 @@ app.post('/noleggio', function (req, res) {
             MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true', function(err, db) {
               if (err) {throw err;}
               var dbo = db.db("Tecnologie");
-              var newRist = {username:req.body.User, dataInizio: req.body.dataIn, oraInizio: req.body.oraIn , dataFine:req.body.dataFi, oraFine:req.body.oraFi};
+              var newRist = {username:req.body.User, dataInizio: req.body.dataIn, oraInizio: req.body.oraIn , dataFine:req.body.dataFi, oraFine:req.body.oraFi, idUser:req.body.UserId, idMono:req.body.MonoId};
               dbo.collection("Noleggio").insertOne(newRist, function(err, result) {
                 if (err) throw err;
                 db.close();
@@ -79,6 +77,29 @@ app.post('/noleggio', function (req, res) {
     });
 });
 
+app.post('/stato', function (req, res) {
+            MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true', function(err, db) {
+              if (err) {throw err;}
+              var dbo = db.db("Tecnologie");
+              dbo.collection("Monopattini").updateOne({'_id':ObjectID(req.body.MonoId)}, {$set: {stato: req.body.Stato}}, function(err, result) {
+                if (err) throw err;
+                db.close();
+                console.log("ok!")
+              });
+    });
+});
+
+app.post('/guasto', function (req, res) {
+            MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true', function(err, db) {
+              if (err) {throw err;}
+              var dbo = db.db("Tecnologie");
+              var newRist = {idMono:req.body.MonoId, idUser:req.body.UserId, dataSegnalazione:req.body.DataSegn, oraSegnalazione:req.body.OraSegn , problema:req.body.Problema};
+              dbo.collection("Guasti").insertOne(newRist, function(err, result) {
+                if (err) throw err;
+                db.close();
+              });
+    });
+});
 
 
 app.get('/coordinate', function (req, res) {
@@ -95,6 +116,38 @@ app.get('/coordinate', function (req, res) {
       })
     });
 });
+
+
+
+
+app.get('/info/:idInfo', function (req, res) {
+      MongoClient.connect('mongodb+srv://admin:wenandrea123@andreawen-jytg8.mongodb.net/test?retryWrites=true,{useNewUrlParser: true}', function(err, db) {
+      if (err) {
+        throw err;
+      }
+      var dbo = db.db("Tecnologie");
+      dbo.collection("Users").findOne({_id: ObjectID(req.params.idInfo)}, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        if(result){
+            res.send({result:result})
+
+        }else{
+            res.send({result:"Errore"})
+        }
+        db.close();
+      })
+    });
+});
+
+
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
